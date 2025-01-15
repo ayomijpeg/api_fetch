fa3cc1074fae2ac46ceaa57e5cad6cd42c6a416f
ayomijpeg <ayomitomiwa53@gmail.com>
2025-01-15 23:32:48 +0100

    add tailwind

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
+function fetchData() {
+    const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
+    const pokemonSprite = document.getElementById('pokemonSprite');
 
-
-
-async function fetchData() {
-  try {
-         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();  
-          const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-
-      if (!response.ok) {
-          throw new Error("Could not fetch resource");
-      }
-      const data = await response.json();
-     const pokemonSprite = data.sprites.front_default;
-      const imgElement = document.getElementById("pokemonSprite");
-
-      imgElement.src = pokemonSprite;
-      imgElement.style.display = "block";
-  } catch (error) {
-      console.log(error);
-  }
+    if (pokemonName) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+            .then(response => response.json())
+            .then(data => {
+                const spriteUrl = data.sprites.front_default;
+                pokemonSprite.src = spriteUrl;
+                pokemonSprite.style.display = "block";
+            })
+            .catch(error => {
+                alert("Pokemon not found!");
+            });
+    } else {
+        alert("Please enter a Pokemon name!");
+    }
 }
